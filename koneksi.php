bc60911c83ee9b591b1f4cade445293a7c50533b
Beram-BeramLawut <wijaksonk@gmail.com>
2026-01-16 15:07:51 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -6,12 +6,9 @@ $username = "root";
 $password = "";
 $db = "webdailyjournal"; //nama database
 
-//create connection
 $conn = new mysqli($servername,$username,$password,$db);
 
-//check apakah ada error connection
 if($conn->connect_error){
-	//jika ada, hentikan script dan tampilkan pesan error
 	die("Connection failed : ".$conn->connect_error);
 }
 
